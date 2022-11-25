aa497465e6b5400f3dbb38125abf6c2f24c78476
Elijah Odjegba <elijahodjegba@gmail.com>
2022-11-25 18:11:57 +0100

    Add custom methods and fix linters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+  # A method that updates the comments counter for a post.
+  def updates_comments_counter_post
+    posts.increment!(:comments_counter)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
 
+  # A method that returns the 3 most recent posts for a given user.
+  def most_recent_post
+    Post.order(created_at: :desc).limit(3)
+  end
 end
