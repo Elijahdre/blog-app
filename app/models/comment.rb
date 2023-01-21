706100593d147c34dc9bb5d94651c3ffa41978e2
Elijah Odjegba <elijahodjegba@gmail.com>
2023-01-21 20:03:21 +0100

    Add test case for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
   # A method that updates the comments counter for a post.
   def updates_comments_counter_post
-    posts.increment!(:comments_counter)
+    post.increment!(:comments_counter)
   end
 end
