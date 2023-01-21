cc1327f1eb1e08ab26f4c45d31091d190de3eac4
Elijah Odjegba <elijahodjegba@gmail.com>
2023-01-21 20:47:57 +0100

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
-  
-# Validations for the user model
+
+  # Validations for the user model
   validates :name, presence: true, allow_blank: false
   validates :post_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
