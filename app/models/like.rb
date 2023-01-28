ef631269eb26d13a0fa7e082cf01a7f9a7d8391b
Elijah Odjegba <elijahodjegba@gmail.com>
2023-01-28 10:57:30 +0100

    Edit comment model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,12 @@
 class Like < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post, class_name: 'Post', foreign_key: :post_id
 
   # A method that updates the likes counter for a post.
+  after_save :updates_likes_counter_post
+
+  private
+  
   def updates_likes_counter_post
     post.increment!(:likes_counter)
   end
