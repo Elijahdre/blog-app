6025fc7b0430bc9ce671b66a7a83091e8b77de9e
Elijah Odjegba <elijahodjegba@gmail.com>
2023-02-01 12:26:17 +0100

    Solve n + 1

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -1,20 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  user = User.create(name: 'John', posts_counter: 30, photo: 'https://randomuser.me/api/portraits/men/70.jpg',
+  bio: 'Teacher from Nigeria.')
+
+  subject do
+  Post.new(author_id: user.id, title: 'First Post', text: 'My first post', comments_counter: 20, likes_counter: 30)
+  end
+
+  before { subject.save }
+
   describe 'GET /index' do
     it 'returns http success and check correct placeholder text.' do
-      get '/users/:author_id/posts'
+      get user_posts_path(user.id)
       expect(response).to have_http_status(:ok)
-      expect(response.body).to include('Here is a list of posts for a given user')
+      expect(response.body).to include('Posts')
     end
   end
 
   describe 'GET /show' do
     it 'returns http success and check correct placeholder text.' do
-      get '/users/:author_id/posts/:id'
+      get user_post_path(user.id, subject.id)
       expect(response).to have_http_status(:ok)
-      expect(response.body).to include('Here is a specific post')
-      expect(response).to render_template(:show)
+      expect(response.body).to include('Comments')
     end
   end
 end
--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -1,20 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  subject = User.create(name: 'John', post_counter: 30, photo: 'https://randomuser.me/api/portraits/men/70.jpg',
+                        bio: 'Teacher from Nigeria.')
+
+  before { subject.save }
+
   describe 'GET /index' do
     it 'returns http success and check correct placeholder text.' do
       get '/'
       expect(response).to have_http_status(:ok)
-      expect(response.body).to include('Here is a list of Users')
+      expect(response.body).to include('Users')
+
     end
   end
 
   describe 'GET /show' do
     it 'returns http success and check correct placeholder text.' do
-      get '/users/:id'
+      get user_path(subject.id)
       expect(response).to have_http_status(:ok)
-      expect(response.body).to include('Here is a specific user')
-      expect(response).to render_template(:show)
+      expect(response.body).to include(subject.name)
     end
   end
 end
