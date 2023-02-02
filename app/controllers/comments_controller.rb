022e3ce0674a3b9637e1e5d5020496ab10854900
Elijah Odjegba <elijahodjegba@gmail.com>
2023-02-02 18:28:45 +0100

    Edit comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,8 +9,10 @@ class CommentsController < ApplicationController
     @comments.post_id = @current_post.id
     @comments.author_id = current_user.id
     if @comments.save
-      user_posts_path(@current_post.author_id, @current_post.id), notice: 'New comment successfully saved'
+      redirect_to user_posts_path(@current_post.author_id, @current_post.id), 
+      notice: 'New comment saved successfully'
 
+      
     else
       render :new, status: :unprocessable_entity, alert: 'Something went wrong'
     end
